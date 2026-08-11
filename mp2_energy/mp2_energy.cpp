1e01429bb6919772a5f3f2415eb8ab4134b1ff2d
Alyson MacKay <alyson.mackay@mail.concordia.ca>
2026-08-11 11:56:07 -0400

    moved ao2mo transform to function

--- mp2_energy/mp2_energy.cpp
+++ mp2_energy/mp2_energy.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
@@ -9,35 +10,10 @@ static vector<int> ioff;
 static int compound(int a, int b) {
 	return a>b ? ioff[a] + b : ioff[b] + a;
 }
-
-int main(int argc, char* argv[]) {
-
-	// run HF SCF to get converged MOs
-	Result conv_orbs = run_scf(argv[1], argv[2]);
-	printf("\nConverged MO Coefficient Matrix:\n");
-	print_matrix(conv_orbs.C);
-	printf("\nConverged MO Energies:\n");
-	cout << conv_orbs.epsi << endl;
-
-	// read two-electron integrals in AO basis
-	int nao = conv_orbs.C.rows();
-	ifstream two_elec("../input/" + string(argv[1]) + "/" + argv[2] + "/eri.dat"); // kinda messy 
-	int M = nao*(nao+1)/2; // number of distinict pairs
-	ioff.resize(M);
-	ioff[0] = 0; 
-	for(int k=1; k < M; k++)
-		ioff[k] = ioff[k-1] +k;
-	int size = compound(M-1, M-1) + 1; 
-	vector<double> TEI_AO(size);
-	int mu, nu, la, si = 0; // AO indices 
-	double x;
-	while(two_elec >> mu >> nu >> la >> si >> x) { 
-		TEI_AO[ compound( compound(mu-1,nu-1), compound(la-1,si-1) ) ] = x; 
-	}
-
-	// transform TEI_AO to MO basis 
+// transform TEI_AO to MO basis 
+vector<double> ao2mo_transform(const vector<double>& TEI_AO, const Matrix& coeff, int nao) {
+	int size = TEI_AO.size();
 	vector<double> TEI_MO(size);
-	// first attempt: Noddy algorithm (N^8)
 	int i, j, k, l, ijkl; // MO indices 
 	int p, q, r, s, pq, rs, pqrs; // AO indices
 	for(i=0, ijkl=0; i < nao; i++) {
@@ -53,7 +29,7 @@ int main(int argc, char* argv[]) {
 									rs = compound(r,s); 
 									pqrs = compound(pq, rs); 
 									
-									TEI_MO[ijkl] += conv_orbs.C(p,i) * conv_orbs.C(q,j) * conv_orbs.C(r,k) * conv_orbs.C(s,l) * TEI_AO[pqrs];
+									TEI_MO[ijkl] += coeff(p,i) * coeff(q,j) * coeff(r,k) * coeff(s,l) * TEI_AO[pqrs];
 								}
 							}
 						}
@@ -62,10 +38,46 @@ int main(int argc, char* argv[]) {
 			}
 		}
         }
+	return TEI_MO;
+}
 
+
+
+int main(int argc, char* argv[]) {
+
+	// run HF SCF to get converged MOs
+	Result conv_orbs = run_scf(argv[1], argv[2]);
+	printf("\nConverged MO Coefficient Matrix:\n");
+	print_matrix(conv_orbs.C);
+	printf("\nConverged MO Energies:\n");
+	cout << conv_orbs.epsi << endl;
+	double E_scf = conv_orbs.Escf; 
+
+	// read two-electron integrals in AO basis
+	int nao = conv_orbs.C.rows();
+	ifstream two_elec("../input/" + string(argv[1]) + "/" + argv[2] + "/eri.dat"); // kinda messy 
+	int M = nao*(nao+1)/2; // number of distinict pairs
+	ioff.resize(M);
+	ioff[0] = 0; 
+	for(int k=1; k < M; k++)
+		ioff[k] = ioff[k-1] +k;
+	int size = compound(M-1, M-1) + 1; 
+	vector<double> TEI_AO(size);
+	int mu, nu, la, si; // AO indices 
+	double x;
+	while(two_elec >> mu >> nu >> la >> si >> x) { 
+		TEI_AO[ compound( compound(mu-1,nu-1), compound(la-1,si-1) ) ] = x; 
+	}
+
+	auto t0 = chrono::steady_clock::now(); 
+	vector<double> TEI_MO = ao2mo_transform(TEI_AO, conv_orbs.C, nao);
+	auto t1 = chrono::steady_clock::now();
+	chrono::duration<double> dt = t1 - t0;
+	printf("\nTime for AO2MO Transformation: %.12f s\n", dt.count());
+	
 	double Emp2 = 0.0;
-	int ndocc = conv_orbs.nocc; // assume only closed 
-        int a, b, ia, ja, jb, ib, iajb, ibja; // i and j are doubly occupied; a and b unoccupied
+	int ndocc = conv_orbs.nocc; // assume only closed shell for now
+	int i, j, a, b, ia, ja, jb, ib, iajb, ibja; // i and j are doubly occupied; a and b unoccupied
 	for(i=0; i < ndocc; i++) {
 		for(a=ndocc; a < nao; a++) {
 			ia = compound(i,a);
@@ -74,16 +86,18 @@ int main(int argc, char* argv[]) {
 				for(b=ndocc; b < nao; b++) {
 					jb = compound(j,b); 
 					ib = compound(i,b); 
-					iajb = compound(ia,jb); 
-					ibja = compound(ib,ja); 
+					iajb = compound(ia,jb);
+					ibja = compound(ib,ja);
 					Emp2 += TEI_MO[iajb] * (2 * TEI_MO[iajb] - TEI_MO[ibja]) / (conv_orbs.epsi[i] + conv_orbs.epsi[j] - conv_orbs.epsi[a] - conv_orbs.epsi[b]);
 					
 				}
 			}
 		}
 	}
-	printf("\nMP2 Energy:");
-	cout << Emp2 << endl;
+	printf("\nSCF Energy: %.12f\n", E_scf); 
+	printf("\nMP2 Energy: %.12f\n", Emp2);
+	double E_tot = E_scf + Emp2; 
+	printf("\nTotal Energy: %.12f\n", E_tot);
 	
 	return 0;
 }
